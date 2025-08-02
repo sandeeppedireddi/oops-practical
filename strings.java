7ef6c0c7eb8e257e1131b20c8769bb9c7efde7da
sandeep <sandeeppedireddi@gmail.com>
2025-08-02 10:46:59 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class strings {
+	public static void main(String[] args) {
+		String s = "Hello_world";
+		System.out.println(s.indexOf("_"));
+		System.out.println(s.lastIndexOf("or"));
+		System.out.println(s.contains("ll"));
+	}
+          
+}
