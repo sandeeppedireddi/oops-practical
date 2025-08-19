bdf0b55b7cba4b615741ac480335fdb57672e783
sandeep <sandeeppedireddi@gmail.com>
2025-08-19 14:31:48 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,23 @@
+package oops;
+class Vehicle {
+    int maxSpeed = 120;
+}
+
+class Car extends Vehicle {
+    int maxSpeed = 180;
+    void display()
+    {
+        
+        System.out.println("Maximum Speed: "
+                           + super.maxSpeed);
+    }
+}
+
+public class Test {
+	public static void main(String[] args)
+    {
+        Car small = new Car();
+        small.display();
+    }
+
+}
